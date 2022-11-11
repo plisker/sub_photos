92faa25701fa394cc99f3a7d071bd817f9430e96
Paul Lisker <plisker@gmail.com>
2022-11-11 15:25:45 -0500

    accessibility and cleanup

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@
         // By defailt it looks for an image tag:
         opener: function (openerElement) {
           // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          // you don't need to add "opener" option if this code matches your needs, it's default one.
           return openerElement.is("img")
             ? openerElement
             : openerElement.find("img");
