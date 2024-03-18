05d332448f8c30499ed056f9809f7ce1a5bfce15
Paul Lisker <plisker@gmail.com>
2024-03-18 14:37:44 -0500

    add footer with JS

--- js/photos.js
+++ js/photos.js
@@ -66,8 +66,7 @@ function populatePhotoGrid(jsonFilePath, offset = 0) {
             });
 
             // Append footer to the photo-grid
-            const footer = document.getElementById('fh5co-footer');
-            currentColumn.appendChild(footer);
+            currentColumn.appendChild(getFooter());
 
             // Append columns to the photo-grid
             photoGrid.appendChild(column1);
@@ -76,4 +75,45 @@ function populatePhotoGrid(jsonFilePath, offset = 0) {
             lightbox();
         })
         .catch(error => console.error('Error fetching photos:', error));
+}
+
+function getFooter() {
+    let footer = document.createElement('div');
+    footer.className = 'padding-left';
+    footer.id = 'fh5co-footer';
+
+    footer.innerHTML = `
+        <p>
+            <small>&copy; 2024 Paul Lisker. All Rights Reserved.<br />
+                <a href="https://lisker.me/privacy_policy" target="_blank">Privacy Policy</a>
+                &#183;
+                <a href="https://lisker.me/cookie_policy" target="_blank">Cookie Policy</a><br />
+                Modified from a design by
+                <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a></small>
+        </p>
+        <ul class="fh5co-social">
+            <li>
+                <a href="https://www.instagram.com/paullisker/" rel="noopener" target="_blank"
+                    aria-label="See: Instagram"><i class="fa-brands fa-instagram"></i></a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/paullisker/" rel="noopener" target="_blank"
+                    aria-label="See: LinkedIn"><i class="fa-brands fa-linkedin"></i></a>
+            </li>
+            <li>
+                <a href="https://twitter.com/PaulLisker/" rel="noopener" target="_blank"
+                    aria-label="See: Twitter"><i class="fa-brands fa-twitter"></i></a>
+            </li>
+            <li>
+                <a href="https://ebird.org/profile/MTgxMjE3NQ/" rel="noopener" target="_blank"
+                    aria-label="See: eBird"><i class="fa-solid fa-binoculars"></i></a>
+            </li>
+            <li>
+                <a href="mailto:photos@lisker.me/" rel="noopener" target="_blank"
+                    aria-label="Email me: photos@lisker.me"><i class="fa-regular fa-envelope"></i></a>
+            </li>
+        </ul>
+    `;
+
+    return footer;
 }
