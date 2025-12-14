ed3ce7916eba993a7220e146b3203ec0644c9d8d
Paul Lisker <plisker@gmail.com>
2025-12-13 21:00:54 -0600

    Website Updates Dec 2025 (#10)
    * add tallinn photo
    
    * format
    
    * format and snowy owls
    
    * remove scripts
    
    * add nickerson and ri photos
    
    * add spiders
    
    * add locations to wildlife
    
    * add locations to birds of prey
    
    * add locations to backyard birds
    
    * add locations to america birds
    
    * format json
    
    * fix camera icon loading issue and format js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,109 +1,110 @@
 function lightbox() {
-    "use strict";
+  "use strict";
 
-    // Loading page
-    //   var loaderPage = function () {
-    //     $(".fh5co-loader").fadeOut("slow");
-    //   };
+  // Loading page
+  //   var loaderPage = function () {
+  //     $(".fh5co-loader").fadeOut("slow");
+  //   };
 
-    // Magnific Popup
-    var magnifPopup = function () {
-        $(".image-popup").magnificPopup({
-            type: "image",
-            removalDelay: 300,
-            mainClass: "mfp-with-zoom",
-            titleSrc: "title",
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0, 1],
-            },
-            zoom: {
-                enabled: true,
-                duration: 300,
-                easing: "ease-in-out",
-                opener: function (openerElement) {
-                    return openerElement.is("img")
-                        ? openerElement
-                        : openerElement.find("img");
-                },
-            },
-            callbacks: {
-                open: function () {
-                    // Update the URL when the lightbox opens with the current photo
-                    var currentPhoto = this.currItem.el.attr('href');
-                    var photoId = extractPhotoId(currentPhoto);
+  // Magnific Popup
+  var magnifPopup = function () {
+    $(".image-popup").magnificPopup({
+      type: "image",
+      removalDelay: 300,
+      mainClass: "mfp-with-zoom",
+      titleSrc: "title",
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1],
+      },
+      zoom: {
+        enabled: true,
+        duration: 300,
+        easing: "ease-in-out",
+        opener: function (openerElement) {
+          return openerElement.is("img")
+            ? openerElement
+            : openerElement.find("img");
+        },
+      },
+      callbacks: {
+        open: function () {
+          // Update the URL when the lightbox opens with the current photo
+          var currentPhoto = this.currItem.el.attr("href");
+          var photoId = extractPhotoId(currentPhoto);
 
-                    var newUrl = window.location.pathname + '#photo=' + encodeURIComponent(photoId);
-                    history.pushState(null, null, newUrl);
-                },
-                change: function () {
-                    // Update the URL when navigating to a new photo
-                    var currentPhoto = this.currItem.el.attr('href');
-                    var photoId = extractPhotoId(currentPhoto);
+          var newUrl =
+            window.location.pathname + "#photo=" + encodeURIComponent(photoId);
+          history.pushState(null, null, newUrl);
+        },
+        change: function () {
+          // Update the URL when navigating to a new photo
+          var currentPhoto = this.currItem.el.attr("href");
+          var photoId = extractPhotoId(currentPhoto);
 
-                    var newUrl = window.location.pathname + '#photo=' + encodeURIComponent(photoId);
-                    history.pushState(null, null, newUrl);
-                },
-                close: function () {
-                    // Restore the original URL when the popup is closed
-                    var cleanUrl = window.location.pathname;
-                    history.pushState(null, null, cleanUrl);
-                }
-            }
-        });
-    };
-    
-
-    var contentWayPoint = function () {
-        var i = 0;
-        $(".animate-box").waypoint(
-            function (direction) {
-                if (direction === "down" && !$(this.element).hasClass("animated")) {
-                    i++;
-
-                    $(this.element).addClass("item-animate");
-                    setTimeout(function () {
-                        $("body .animate-box.item-animate").each(function (k) {
-                            var el = $(this);
-                            setTimeout(
-                                function () {
-                                    el.addClass("fadeIn animated");
-                                    el.removeClass("item-animate");
-                                },
-                                k * 200,
-                                "easeInOutExpo"
-                            );
-                        });
-                    }, 100);
-                }
-            },
-            { offset: "50%" }
-        );
-    };
-
-    // Helper function to extract unique photo ID from a full photo path
-    function extractPhotoId(photoPath) {
-        // Use regex to extract the file name without the extension
-        var match = photoPath.match(/\/([^\/]+)\.\w+$/);
-        return match ? match[1] : photoPath; // Return the file name if matched, or the original path if not
-    }
-
-    // Document on load.
-    $(function () {
-        // loaderPage();
-        magnifPopup();
-
-        // Animations
-        // contentWayPoint();
+          var newUrl =
+            window.location.pathname + "#photo=" + encodeURIComponent(photoId);
+          history.pushState(null, null, newUrl);
+        },
+        close: function () {
+          // Restore the original URL when the popup is closed
+          var cleanUrl = window.location.pathname;
+          history.pushState(null, null, cleanUrl);
+        },
+      },
     });
-};
+  };
+
+  var contentWayPoint = function () {
+    var i = 0;
+    $(".animate-box").waypoint(
+      function (direction) {
+        if (direction === "down" && !$(this.element).hasClass("animated")) {
+          i++;
+
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .animate-box.item-animate").each(function (k) {
+              var el = $(this);
+              setTimeout(
+                function () {
+                  el.addClass("fadeIn animated");
+                  el.removeClass("item-animate");
+                },
+                k * 200,
+                "easeInOutExpo"
+              );
+            });
+          }, 100);
+        }
+      },
+      { offset: "50%" }
+    );
+  };
+
+  // Helper function to extract unique photo ID from a full photo path
+  function extractPhotoId(photoPath) {
+    // Use regex to extract the file name without the extension
+    var match = photoPath.match(/\/([^\/]+)\.\w+$/);
+    return match ? match[1] : photoPath; // Return the file name if matched, or the original path if not
+  }
+
+  // Document on load.
+  $(function () {
+    // loaderPage();
+    magnifPopup();
+
+    // Animations
+    // contentWayPoint();
+  });
+}
 
 function getSocials() {
-    let socials = document.createElement('ul');
-    socials.className = 'fh5co-social';
+  let socials = document.createElement("ul");
+  socials.className = "fh5co-social";
 
-    socials.innerHTML = `
+  socials.innerHTML = `
             <li>
                 <a href="https://www.instagram.com/paullisker/" rel="noopener" target="_blank"
                     aria-label="See: Instagram"><i class="fa-brands fa-instagram"></i></a>
@@ -126,17 +127,17 @@ function getSocials() {
             </li>
     `;
 
-    return socials;
+  return socials;
 }
 
 function getFooter() {
-    let footer = document.createElement('div');
-    footer.className = 'padding-left';
-    footer.id = 'fh5co-footer';
+  let footer = document.createElement("div");
+  footer.className = "padding-left";
+  footer.id = "fh5co-footer";
 
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-    footer.innerHTML = `
+  footer.innerHTML = `
         <p>
             <small>&copy; ${currentYear} Paul Lisker. All Rights Reserved.<br />
                 <a href="https://lisker.me/privacy_policy" target="_blank">Privacy Policy</a>
@@ -147,38 +148,60 @@ function getFooter() {
         </p>
     `;
 
-    footer.appendChild(getSocials());
+  footer.appendChild(getSocials());
 
-    return footer;
+  return footer;
 }
 
-
 $(document).ready(function () {
-    const socials = document.getElementById('socials');
-    if (socials) {
-        socials.insertAdjacentElement('beforeend', getSocials());
-    }
+  // Wait for Font Awesome to load and convert icons to SVGs
+  function waitForFontAwesome() {
+    // Check if Font Awesome has converted the icons to SVGs
+    var checkInterval = setInterval(function () {
+      var logo = document.getElementById("fh5co-logo");
+      if (logo && logo.querySelector("svg.svg-inline--fa")) {
+        logo.classList.add("fa-loaded");
+        clearInterval(checkInterval);
+      }
+    }, 10);
 
-    const footer = document.getElementById('footer');
-    if (footer) {
-        footer.insertAdjacentElement('beforeend', getFooter());
-    }
+    // Failsafe: show icons after 500ms even if Font Awesome hasn't loaded
+    setTimeout(function () {
+      var logo = document.getElementById("fh5co-logo");
+      if (logo) {
+        logo.classList.add("fa-loaded");
+      }
+      clearInterval(checkInterval);
+    }, 500);
+  }
 
-    $("body").css("display", "none");
+  waitForFontAwesome();
 
-    $("body").fadeIn(2000).stop().animate({ opacity: 1 });
+  const socials = document.getElementById("socials");
+  if (socials) {
+    socials.insertAdjacentElement("beforeend", getSocials());
+  }
 
-    // Check for a photo hash in the URL
-    var hash = window.location.hash;
-    if (hash && hash.includes('#photo=')) {
-        var photoUrl = decodeURIComponent(hash.split('#photo=')[1]);
-        
-        // Automatically open the lightbox for the specific photo
-        $.magnificPopup.open({
-            items: {
-                src: photoUrl
-            },
-            type: 'image'
-        });
-    }
+  const footer = document.getElementById("footer");
+  if (footer) {
+    footer.insertAdjacentElement("beforeend", getFooter());
+  }
+
+  $("body").css("display", "none");
+
+  $("body").fadeIn(2000).stop().animate({ opacity: 1 });
+
+  // Check for a photo hash in the URL
+  var hash = window.location.hash;
+  if (hash && hash.includes("#photo=")) {
+    var photoUrl = decodeURIComponent(hash.split("#photo=")[1]);
+
+    // Automatically open the lightbox for the specific photo
+    $.magnificPopup.open({
+      items: {
+        src: photoUrl,
+      },
+      type: "image",
     });
+  }
+});
